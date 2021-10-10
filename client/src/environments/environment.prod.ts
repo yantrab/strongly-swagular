a27fbaa5b9fe9466c8e47f3512a9f5d43d544ab7
yantrab <yanivt@traxretail.com>
2021-10-10 15:36:37 +0300

    progress bar

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://?:3000',
-  socketUrl: 'http://localhost:4001'
+  apiUrl: 'http://128.199.41.162:3000',
+  socketUrl: 'http://128.199.41.162:4001'
 };
