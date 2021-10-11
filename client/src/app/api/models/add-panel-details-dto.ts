755022e494037fdb8988fe92f3075782726017bc
yantrab <yanivt@traxretail.com>
2021-10-11 10:06:34 +0300

    progress bar

--- client/src/app/api/models/add-panel-details-dto.ts
+++ client/src/app/api/models/add-panel-details-dto.ts
@@ -2,11 +2,11 @@ import { Lang } from './lang';
 export interface AddPanelDetailsDto {
   address: string;
   contactName?: string;
-  contactPhone?: number;
+  contactPhone?: string;
   direction: Lang;
   id: number;
   panelId: number;
-  phoneNumber: number;
+  phoneNumber: string;
 }
 
-export const AddPanelDetailsDtoSchema  = {"type":"object","properties":{"id":{"type":"number"},"phoneNumber":{"type":"number"},"address":{"type":"string"},"contactName":{"type":"string"},"contactPhone":{"type":"number"},"direction":{"$ref":"#/components/schemas/Lang"},"panelId":{"type":"number"}},"required":["id","phoneNumber","address","direction","panelId"]}
+export const AddPanelDetailsDtoSchema  = {"type":"object","properties":{"id":{"type":"number"},"phoneNumber":{"type":"string"},"address":{"type":"string"},"contactName":{"type":"string"},"contactPhone":{"type":"string"},"direction":{"$ref":"#/components/schemas/Lang"},"panelId":{"type":"number"}},"required":["id","phoneNumber","address","direction","panelId"]}
