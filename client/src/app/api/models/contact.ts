6ad20a036fac676ec50d8d9907906e6537b4d548
yantrab <yanivt@traxretail.com>
2021-12-10 10:55:18 +0200

    fix phone numbers

--- client/src/app/api/models/contact.ts
+++ client/src/app/api/models/contact.ts
@@ -8,9 +8,6 @@ export interface Contact {
   tel1?: string;
   tel2?: string;
   tel3?: string;
-  tel4?: string;
-  tel5?: string;
-  tel6?: string;
 }
 
-export const ContactSchema  = {"type":"object","properties":{"index":{"type":"number"},"name1":{"type":"string","maxLength":13},"name2":{"type":"string","maxLength":13},"tel1":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"tel2":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"tel3":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"tel4":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"tel5":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"tel6":{"type":"string","maxLength":15,"pattern":"^[0-9f\\-*#]*$"},"code":{"type":"string","maxLength":10,"pattern":"^[0-9]*$"},"ref":{"type":"string","maxLength":3,"pattern":"^[0-9]*$"},"apartment":{"type":"string","maxLength":3,"pattern":"^[0-9]*$"}},"required":["index","ref","apartment"]}
+export const ContactSchema  = {"type":"object","properties":{"index":{"type":"number"},"name1":{"type":"string","maxLength":13},"name2":{"type":"string","maxLength":13},"tel1":{"type":"string","maxLength":15,"pattern":"^[0-9]*$"},"tel2":{"type":"string","maxLength":15,"pattern":"^[0-9]*$"},"tel3":{"type":"string","maxLength":15,"pattern":"^[0-9]*$"},"code":{"type":"string","maxLength":10,"pattern":"^[0-9]*$"},"ref":{"type":"string","maxLength":3,"pattern":"^[0-9]*$"},"apartment":{"type":"string","maxLength":3,"pattern":"^[0-9]*$"}},"required":["index","ref","apartment"]}
